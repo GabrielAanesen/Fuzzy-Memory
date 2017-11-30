d5bd5a8195e5ff5df788b6c3bcfd6ba3c33bf8ab
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2017-11-30 15:38:00 +0100

    exprimented with images

--- main.js
+++ main.js
@@ -2,32 +2,23 @@ const cards = document.querySelectorAll('.card');
 const button = document.querySelector('button');
 
 var container = document.querySelector('.container')
-var emptyarray = [];
-var complete = [];
-var counter = 0;
+emptyarray = [];
+complete = [];
+let counter = 0;
 
 for (var i = container.children.length; i >= 0; i--) {
        container.appendChild(container.children[Math.random() * i | 0]);
      }
 
 Array.from(cards).forEach( (card)=>{
-  console.log(counter);
-  button.addEventListener('click', (event)=>{
-    card.classList.remove("flip");
-    card.classList.remove("showNum");
-    card.classList.remove("completed");
-    counter = 0;
-    for (var i = container.children.length; i >= 0; i--) {
-           container.appendChild(container.children[Math.random() * i | 0]);
-         }
-    })
+
   card.addEventListener('click', (event)=>{
-    emptyarray.push(event.target.dataset.card);
-    complete.push(card);
     card.classList.add("flip");
     card.classList.add("showNum");
-    if (emptyarray.length === 2) {
-      if (emptyarray[0] === emptyarray[1]) {
+    emptyarray.push(event.target.dataset.card);
+    complete.push(card);
+    if (emptyarray.length == 2) {
+      if (emptyarray[0] == emptyarray[1]) {
             complete[0].classList.add("completed");
             complete[1].classList.add("completed");
             emptyarray = [];
@@ -51,25 +42,25 @@ Array.from(cards).forEach( (card)=>{
       }
     }
   })
+  button.addEventListener('click', (event)=>{
+    card.classList.remove("flip");
+    card.classList.remove("showNum");
+    card.classList.remove("completed");
+    emptyarray = [];
+    complete = [];
+    counter = 0;
+    container.classList.add("shake-horizontal");
+    setTimeout(function(){
+      container.classList.remove("shake-horizontal");
+    },500);
+
+    setTimeout(function(){
+    for (var i = container.children.length; i >= 0; i--) {
+           container.appendChild(container.children[Math.random() * i | 0]);
+         }
+       },500);
+    })
 })
 
-// const divArray = [
-//   '<div class="card" data-card="1"><p>1</p></div>',
-//   '<div class="card" data-card="1"><p>1</p></div>',
-//   '<div class="card" data-card="2"><p>2</p></div>',
-//   '<div class="card" data-card="2"><p>2</p></div>',
-//   '<div class="card" data-card="3"><p>3</p></div>',
-//   '<div class="card" data-card="3"><p>3</p></div>',
-//   '<div class="card" data-card="4"><p>4</p></div>',
-//   '<div class="card" data-card="4"><p>4</p></div>',
-//   '<div class="card" data-card="5"><p>5</p></div>',
-//   '<div class="card" data-card="5"><p>5</p></div>',
-//   '<div class="card" data-card="6"><p>6</p></div>',
-//   '<div class="card" data-card="6"><p>6</p></div>',
-//   '<div class="card" data-card="7"><p>7</p></div>',
-//   '<div class="card" data-card="7"><p>7</p></div>',
-//   '<div class="card" data-card="8"><p>8</p></div>',
-//   '<div class="card" data-card="8"><p>8</p></div>',
-//   '<div class="card" data-card="9"><p>9</p></div>',
-//   '<div class="card" data-card="9"><p>9</p></div>'
-// ]
+var emptyarray = [];
+var complete = [];
