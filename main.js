018b4ac298bc41517aca24b6ddbc9c30c99dd1d4
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2017-12-01 09:48:23 +0100

    working out a bug, or trying at least

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 const cards = document.querySelectorAll('.card');
 const button = document.querySelector('button');
 
+const cardsArray = Array.from(cards);
 var container = document.querySelector('.container')
-emptyarray = [];
-complete = [];
+
 let counter = 0;
 
 for (var i = container.children.length; i >= 0; i--) {
        container.appendChild(container.children[Math.random() * i | 0]);
      }
 
-Array.from(cards).forEach( (card)=>{
-
-  card.addEventListener('click', (event)=>{
+cardsArray.forEach( (card)=>{
+  card.addEventListener('click', ()=>{
     card.classList.add("flip");
     card.classList.add("showNum");
-    emptyarray.push(event.target.dataset.card);
+  });
+})
+
+emptyarray = [];
+complete = [];
+counter = 0;
+
+cardsArray.forEach( (card)=>{
+  card.addEventListener('click', (event)=>{
+    console.log(event.target.dataset.number);
+    emptyarray.push(event.target.dataset.number);
     complete.push(card);
-    if (emptyarray.length == 2) {
-      if (emptyarray[0] == emptyarray[1]) {
+    // console.log(emptyarray);
+    if (emptyarray.length === 2) {
+      if (emptyarray[0] === emptyarray[1]) {
             complete[0].classList.add("completed");
             complete[1].classList.add("completed");
             emptyarray = [];
@@ -32,35 +42,34 @@ Array.from(cards).forEach( (card)=>{
             }
       } else {
           setTimeout(function(){
-            complete[1].classList.remove("flip");
-            complete[1].classList.remove("showNum");
             complete[0].classList.remove("showNum");
             complete[0].classList.remove("flip");
+            complete[1].classList.remove("flip");
+            complete[1].classList.remove("showNum");
             emptyarray = [];
             complete = [];
         },500);
       }
     }
   })
-  button.addEventListener('click', (event)=>{
-    card.classList.remove("flip");
-    card.classList.remove("showNum");
-    card.classList.remove("completed");
-    emptyarray = [];
-    complete = [];
-    counter = 0;
-    container.classList.add("shake-horizontal");
-    setTimeout(function(){
-      container.classList.remove("shake-horizontal");
-    },500);
-
-    setTimeout(function(){
-    for (var i = container.children.length; i >= 0; i--) {
-           container.appendChild(container.children[Math.random() * i | 0]);
-         }
-       },500);
-    })
 })
+cardsArray.forEach( (card)=>{
+button.addEventListener('click', (event)=>{
+  card.classList.remove("flip");
+  card.classList.remove("showNum");
+  card.classList.remove("completed");
+  emptyarray = [];
+  complete = [];
+  counter = 0;
+  container.classList.add("shake-horizontal");
+  setTimeout(function(){
+    container.classList.remove("shake-horizontal");
+  },500);
 
-var emptyarray = [];
-var complete = [];
+  setTimeout(function(){
+  for (var i = container.children.length; i >= 0; i--) {
+         container.appendChild(container.children[Math.random() * i | 0]);
+       }
+     },500);
+  })
+})
